ec8850fc60d972cf99b6d5ad5ac89b06f51b8354
M1ngD0ng <alswjd6807@naver.com>
2025-07-25 08:41:34 +0900

    feat : article 관련 뷰 구성

--- board_project/boards/views.py
+++ board_project/boards/views.py
@@ -1,6 +1,36 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
+
+from .models import *
 
 
 def index(request):
     return HttpResponse("Hello World!")
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+
+
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
+    model = Article
+
+
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
+    model = Article
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+
+
+class ArticleListView(ListView):
+    model = Article
