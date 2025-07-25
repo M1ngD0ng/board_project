6df7cb6c2e824883193d1eba8f8a9416ef64970b
M1ngD0ng <alswjd6807@naver.com>
2025-07-25 14:38:14 +0900

    feat : 댓글 생성, 조회 기능 구현

--- board_project/boards/urls.py
+++ board_project/boards/urls.py
@@ -28,4 +28,9 @@ urlpatterns = [
         ArticleDeleteView.as_view(),
         name="article-delete",
     ),
+    path(
+        "articles/<int:article_id>/comment/",
+        CommentCreateView.as_view(),
+        name="comment-form",
+    ),
 ]
--- board_project/boards/views.py
+++ board_project/boards/views.py
@@ -10,6 +10,7 @@ from django.views.generic import (
     UpdateView,
 )
 
+from .forms import CommentForm
 from .models import *
 
 
@@ -65,7 +66,13 @@ class ArticleDeleteView(LoginRequiredMixin, DeleteView):
 
 class ArticleDetailView(DetailView):
     model = Article
+    template_name = "boards/article_detail.html"
     context_object_name = "article"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
 
 
 class ArticleListView(ListView):
@@ -82,3 +89,19 @@ class ArticleListView(ListView):
         context = super().get_context_data(**kwargs)
         context["board"] = Board.objects.get(id=self.kwargs["board_id"])
         return context
+
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        article = Article.objects.get(id=self.kwargs["article_id"])
+
+        form.instance.article = article
+        form.instance.author = self.request.user
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("boards:article-detail", kwargs={"pk": self.object.article.pk})
