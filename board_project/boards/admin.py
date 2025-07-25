7d07f767c08c103349b7227b9aa3907f7ec3acf2
M1ngD0ng <alswjd6807@naver.com>
2025-07-25 10:55:28 +0900

    mod : 카테고리 모델 삭제

--- board_project/boards/admin.py
+++ board_project/boards/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import Board, Category, Article, Comment
+from .models import Article, Board, Comment
 
 admin.site.register(Board)
-admin.site.register(Category)
 admin.site.register(Article)
 admin.site.register(Comment)
--- board_project/boards/models.py
+++ board_project/boards/models.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.db import models
 
-
 # Create your models here.
+
 
 class Board(models.Model):
     name = models.CharField(max_length=100)
@@ -12,41 +12,30 @@ class Board(models.Model):
         return self.name
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    category = models.ForeignKey(
-        Category,
+    board = models.ForeignKey(
+        Board,
         on_delete=models.PROTECT,
-        related_name="articles", )
+        related_name="articles",
+    )
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="articles")
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="articles"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"[{self.category.name}] {self.title}"
+        return f"[{self.board.name}] {self.title}"
 
 
 class Comment(models.Model):
     article = models.ForeignKey(
-        Article,
-        on_delete=models.CASCADE,
-        related_name="comments"
+        Article, on_delete=models.CASCADE, related_name="comments"
     )
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="comments"
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
     )
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
