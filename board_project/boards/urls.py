3af53a79067e94160d039a509e1567cb83e62105
M1ngD0ng <alswjd6807@naver.com>
2025-07-25 11:04:30 +0900

    feat : board 관련 url 등록

--- board_project/boards/urls.py
+++ board_project/boards/urls.py
@@ -12,4 +12,22 @@ urlpatterns = [
         ArticleListView.as_view(),
         name="article-list",
     ),
+    path(
+        "<int:board_id>/articles/", ArticleCreateView.as_view(), name="article-create"
+    ),
+    path(
+        "<int:board_id>/articles/<int:article_id>",
+        ArticleDetailView.as_view(),
+        name="article-detail",
+    ),
+    path(
+        "<int:board_id>/articles/<int:article_id>",
+        ArticleUpdateView.as_view(),
+        name="article-update",
+    ),
+    path(
+        "<int:board_id>/articles/<int:article_id>",
+        ArticleDeleteView.as_view(),
+        name="article-delete",
+    ),
 ]
