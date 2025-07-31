dffee339694ef13cd0b700f8a82ffc5175167d2c
M1ngD0ng <alswjd6807@naver.com>
2025-07-31 17:46:13 +0900

    feat : django-suit-v2 초기 구성

---
+++ board_project/boards/commons/apps.py
@@ -0,0 +1,30 @@
+from django.apps import AppConfig
+from suit.apps import DjangoSuitConfig
+from suit.menu import ChildItem, ParentItem
+
+
+class CommonsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "board_project.boards.commons"
+
+
+class SuitConfig(DjangoSuitConfig):
+    menu = (
+        ParentItem(
+            "사용자 관리",
+            children=[
+                ChildItem(model="users.user"),
+                ChildItem(model="auth.group"),
+            ],
+            icon="fa fa-users",
+        ),
+        ParentItem(
+            "게시판 관리",
+            children=[
+                ChildItem(model="boards.board"),
+                ChildItem(model="boards.article"),
+                ChildItem(model="boards.comment"),
+            ],
+            icon="fa fa-list",
+        ),
+    )
