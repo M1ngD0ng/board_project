3ee5c3139f6240123830ea52d0ed130cac172523
M1ngD0ng <alswjd6807@naver.com>
2025-08-01 11:09:46 +0900

    mod : admin 클래스 공통 부분 클래스 생성

---
+++ board_project/boards/commons/admin.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib import admin
+from django.db import models
+
+
+class SiteBaseModelAdmin(admin.ModelAdmin):
+    formfield_overrides = (
+        {
+            models.TextField:{"widget": forms.Textarea(attrs={"rows": 4, "cols": 80})},
+            models.CharField: {"widget": forms.TextInput(attrs={"class": "form-control"})},
+        }
+    )
+
+    list_per_page = 50
+    list_max_show_all = 500
+
+    save_as = True
+    save_as_continue = False
+    save_on_top = True
+    save_on_bottom = False
+
+    preserve_filters = True
+
+    actions_on_top = False
+    actions_on_bottom = True
